ef9fa30f9df04c64ee25eaac2eb8b4b69550c5e7
WaleedHassanien <waleed121095@gmail.com>
2020-06-17 00:21:21 +0200

    first commit

---
+++ signalrChat/hubs/chat.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signalrChat.hubs
+{
+    public class chat:Hub
+    {
+        public async Task sendMessage(string fromUser,string Message)
+        {
+            await Clients.All.SendAsync("Receive", fromUser, Message);
+        }
+    }
+}
---
+++ signalrChat/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿var connect = new signalR.HubConnectionBuilder().withUrl("/chat").build();
+connect.on("Receive", function (userName, Message) {
+
+    var msg = userName + " " + Message
+    var li = document.createElement("li");
+    li.textContent = msg;
+    $("#listMessage").prepend(li);
+});
+connect.start();
+$("#btnMessage").on("click", function () {
+    var user = $("#txtName").val();
+    var Message = $("#txtMessage").val();
+    connect.invoke("sendMessage", user, Message)
+});
+
